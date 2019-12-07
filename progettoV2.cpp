7daa747c5c559d79067a8e79277ff025cb91be26
filippodaniotti <febbrosto@gmail.com>
2019-12-07 19:00:43 +0100

    Updated comments

--- progettoV2.cpp
+++ progettoV2.cpp
@@ -13,20 +13,28 @@
 
 using namespace std;
 
-// vector to save euler walk
+// Vector to store the euler walk
 vector<int> euler;
-// vector to save depths of vertices in euler walk
+
+// Vector to store the depths of 
+// vertexes in the euler walk
 vector<int> depths;
-// matrix compute the access in O(1) to find operation of the LCA (lowest
-// common ancestor) of a pair of vertexes
+
+// This matrix will compute the access in O(1) to 
+// find operation of the LCA (lowest common ancestor)
+// of a pair of vertexes
 vector<vector<int>> sparseMatrix;
-// hash map to map the vertex to the position in euler walk (index)
+
+// Hash map to map the vertex to the position in euler walk (index)
 unordered_map<int, int> nodesMapping;
-// matrix of cliques where one row is clique and its columns are nodes of the
-// clique
+
+// Matrix of cliques where each row is aclique and 
+// its columns are the nodes of the clique
 vector<vector<int>> cliques;
-// number used to identify the cliques (0 for node not inside clique, 1 for node
-// in first clique, 2 for node in second clique and so on)
+
+// Value used to identify the cliques: 0 for node which 
+// don't belong to any clique, 1 for the nodes in the 
+// first clique, 2 for the nodes in the second clique and so on
 vector<int> isInCliqueNumber;
 
 int counterCliques = 1;
@@ -44,7 +52,7 @@ void printSet(set<int> s) {
 }
 
 /**
- * utility function for union of two set
+ * Utility function to compute the union of two sets
  */
 set<int> setUnion(set<int> a, set<int> b) {
     set<int> c;
@@ -53,7 +61,7 @@ set<int> setUnion(set<int> a, set<int> b) {
 }
 
 /**
- * utility function for difference of two set
+ * Utility function to compute the difference of two sets
  */
 set<int> setDifference(set<int> a, set<int> b) {
     set<int> c;
@@ -63,7 +71,7 @@ set<int> setDifference(set<int> a, set<int> b) {
 }
 
 /**
- * utility function for intersection of two set
+ * Utility function to compute the intersection of two sets
  */
 set<int> setIntersection(set<int> a, set<int> b) {
     set<int> c;
@@ -73,7 +81,7 @@ set<int> setIntersection(set<int> a, set<int> b) {
 }
 
 /**
- * Bron-Kerbosh algorithm two find cliques in undirected graph
+ * Bron-Kerbosh algorithm to find cliques in an undirected graph
  */
 void bronKerbosh(set<int> R, set<int> P, set<int> X,
                  vector<vector<int>> &graph) {
@@ -98,8 +106,8 @@ void bronKerbosh(set<int> R, set<int> P, set<int> X,
 }
 
 /**
- *  recursive part of dfs with pre-order visit to populate euler walk and depths
- * for nodes of the graph
+ * Recursive part of dfs with pre-order visit; this will
+ * populate euler walk and depths for nodes of the graph
  */
 void dfsRec(vector<vector<int>> &graph, int &node, vector<bool> &visited,
             int depth) {
@@ -118,7 +126,8 @@ void dfsRec(vector<vector<int>> &graph, int &node, vector<bool> &visited,
 }
 
 /**
- * function to call the recursion
+ * This procedure just creates a vector to keep track of
+ * which node gets visited and calls the recursion
  */
 void dfs(vector<vector<int>> &graph, int node) {
     vector<bool> visited;
@@ -127,8 +136,9 @@ void dfs(vector<vector<int>> &graph, int node) {
 }
 
 /**
- * fills the matrix[log(euler.size()) + 1][euler.size()] in complexity
- * O(n*log(n)) to compute the LCA (lowest common ancestor) of a pair of vertices
+ * This procedure fills the matrix[log(euler.size()) + 1][euler.size()]
+ * in complexity O(n*log(n)); this will allow to compute the
+ * LCA (lowest common ancestor) of a pair of vertexes
  * in O(1). It takes O(n*log(n)) to store the matrix
  */
 void sparseMatrixComputation() {
@@ -161,11 +171,13 @@ void sparseMatrixComputation() {
 }
 
 /**
- * __builtin_clz function returns the number of zeros before the first occurence
- * of 1 in the binary rapresentation of the given number. Integer in c++ is
- * stored in 32 bits, p contains the position of the most significant bit set
- * to 1. The function uses p to access in the matrix and find the depth of the
- * LCA
+ * The __builtin_clz function returns how many zeros occur
+ * before the first occurence of 1 in the binary representation 
+ * of a given number. Since integers in c++ are 32 bits values, 
+ * we subtract from 31 from the number of zeros to get p, defined
+ * as the position of the most significant bit set to 1. 
+ * The function uses p to access the matrix and 
+ * find the depth of the Lowest Common Ancestor
  */
 int query(int start, int end) {
     int p = 31 - __builtin_clz(end - start);
@@ -173,10 +185,10 @@ int query(int start, int end) {
 }
 
 /**
- * takes in input a pair that represents the start and end point of a request.
+ * This takes as input a pair containing the start and end nodes of a request.
  * It computes in O(1) the position in the euler walk and passes it to
- * query function. This one returns the depths of the LCA so is possibile
- * to satisfy the request
+ * query function, which returns the depths of the LCA and 
+ * allow us to have the request done
  */
 int checkRequest(pair<int, int> request) {
     int start, end;
